45cabaa466854cb6cd0a19f6a32144dfb040b0aa
vitor-risso <vitor.risso13@gmail.com>
2021-02-12 14:11:23 -0300

    api funcionando

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import "reflect-metadata";
+import './utils/connection';
 import { buildSchema } from "type-graphql";
 import { ApolloServer } from 'apollo-server';
 import CategoryResolvers from "./graphql/category/CategoryResolver";
-
-import './utils/connection';
 import VideoResolver from "./graphql/video/VideoResolver";
+
 
 async function bootstrap() {
   const schema = await buildSchema({
