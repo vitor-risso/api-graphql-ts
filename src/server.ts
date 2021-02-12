02a079471e1c8250ee597cc96f153e93e4747210
vitor-risso <vitor.risso13@gmail.com>
2021-02-12 14:01:27 -0300

    adicionando video resolver aos resolvers do type graphql

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ import { ApolloServer } from 'apollo-server';
 import CategoryResolvers from "./graphql/category/CategoryResolver";
 
 import './utils/connection';
+import VideoResolver from "./graphql/video/VideoResolver";
 
 async function bootstrap() {
   const schema = await buildSchema({
-    resolvers: [CategoryResolvers]
+    resolvers: [ CategoryResolvers , VideoResolver]
   });
 
   const server = new ApolloServer({
